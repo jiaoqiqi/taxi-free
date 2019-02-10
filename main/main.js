a5e11dbf1f3f140bc9b0f17342122b7b4f9fd18e
jiaoqiqi <1176932165@qq.com>
2019-02-10 21:35:40 +0800

    get the actul money result
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- main/main.js
+++ main/main.js
@@ -1,4 +1,8 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(kilo, waitMin) {
+
+    let money = 6 + waitMin * 0.25;
+
+    money += (kilo<8) ? (kilo - 2) * 0.8: 4.8 + (kilo - 8) * 1.2;
+
+    console.log(Math.round(money));
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,35 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    var kilo ;
+    var waitMin ;
+    it('公里数在2以内' , function(){
+        kilo = 2;
+        waitMin = 0;
+        spyOn(console , 'log');
+        main(kilo , waitMin);
+        var expectResult = 6;
+
+        expect(console.log).toHaveBeenCalledWith(expectResult);
+    });
+
+    it('公里数在8以内' , function(){
+        kilo = 7;
+        waitMin = 2;
+        spyOn(console , 'log');
+        main(kilo , waitMin);
+        var expectResult = 11;
+
+        expect(console.log).toHaveBeenCalledWith(expectResult);
+    });
+
+    it('公里数大于8' , function(){
+        kilo = 10;
+        waitMin = 20;
+        spyOn(console , 'log');
+        main(kilo , waitMin);
+        var expectResult = 18;
+
+        expect(console.log).toHaveBeenCalledWith(expectResult);
+    });
 });
